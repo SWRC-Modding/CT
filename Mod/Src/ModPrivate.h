d7d1b56be25c9f76ff75ca299bdcf6eb0d5aabaf
Leon0628 <leon@buckel-baehr.de>
2018-08-10 20:23:57 +0200

    Properly set up projects

--- Mod/Src/ModPrivate.h
+++ Mod/Src/ModPrivate.h
@@ -1,5 +1,3 @@
 #pragma once
 
-#include "../../Engine/Inc/Engine.h"
-#include "../../D3DDrv/Inc/D3DDrv.h"
-#include "../Inc/ModClasses.h"
+#include "../Inc/Mod.h"
--- ModMPGame/Src/ModMPGamePrivate.h
+++ ModMPGame/Src/ModMPGamePrivate.h
@@ -1,4 +1,3 @@
 #pragma once
 
-#include "../../Engine/Inc/Engine.h"
-#include "../Inc/ModMPGameClasses.h"
+#include "../Inc/ModMPGame.h"

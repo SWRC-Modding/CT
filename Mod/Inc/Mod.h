cc4ad7d67d3fb9940449afd36843a29d0f51dffe
Leon0628 <leon@buckel-baehr.de>
2018-08-10 20:23:57 +0200

    Properly set up projects

---
+++ Mod/Inc/Mod.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../../Engine/Inc/Engine.h"
+#include "ModClasses.h"
+
+#ifndef MOD_API
+	LINK_LIB(Mod)
+#endif //MOD_API
---
+++ ModMPGame/Inc/ModMPGame.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../../Engine/Inc/Engine.h"
+#include "ModMPGameClasses.h"
+
+#ifndef MODMPGAME_API
+	LINK_LIB(ModMPGame)
+#endif //MODMPGAME_API

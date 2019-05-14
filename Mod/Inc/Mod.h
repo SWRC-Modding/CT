0752b80949b676918cec047a4a8e611e90ba3e81
Leon0628 <leon@buckel-baehr.de>
2019-05-14 13:46:26 +0200

    Remove stuff that doesn't belong here

--- Mod/Inc/Mod.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../../Engine/Inc/Engine.h"
-
-#ifndef MOD_API
-	LINK_LIB(Mod)
-#endif //MOD_API
-
-#include "ModClasses.h"
--- Mod/Src/ModPrivate.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "../Inc/Mod.h"

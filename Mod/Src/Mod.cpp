cd0d79b9577505f2f8eeee8f76b7f4c55bc26174
Leon Buckel <leon@buckel-baehr.de>
2020-03-11 07:17:14 +0100

    Implement bumpmap format conversion for all mip levels

--- Mod/Src/Mod.cpp
+++ Mod/Src/Mod.cpp
@@ -1,3 +1,22 @@
 #include "../Inc/Mod.h"
 
 IMPLEMENT_PACKAGE(Mod)
+
+/*
+ * PatchVTable
+ */
+void* PatchVTable(void** VTable, INT Index, void* Func){
+	DWORD OldProtect;
+
+	if(!VirtualProtect(VTable + Index, sizeof(void*), PAGE_EXECUTE_READWRITE, &OldProtect)){
+		GLog->Logf(NAME_Error, "Unable to patch vtable: VirtualProtect failed with error code %i", GetLastError());
+
+		return NULL;
+	}
+
+	void* OldFunc = VTable[Index];
+
+	VTable[Index] = Func;
+
+	return OldFunc;
+}
--- Mod/Src/ModRenderDevice.cpp
+++ Mod/Src/ModRenderDevice.cpp
@@ -1,97 +1,71 @@
 #include "../../D3DDrv/Inc/D3DDrv.h"
-
+#include "../Inc/Mod.h"
 #include <Windows.h>
 
-/*
- * Patches the given vtable with a custom function and returns the old function
- */
-static void* PatchVTable(void** vtable, INT Index, void* Func){
-	DWORD OldProtect;
-
-	if(!VirtualProtect(vtable, Index * sizeof(void*), PAGE_EXECUTE_READWRITE, &OldProtect)){
-		GLog->Logf(NAME_Error, "Unable to patch vtable: VirtualProtect failed with error code %i", GetLastError());
-
-		return NULL;
-	}
-
-	void* OldFunc = vtable[Index];
-
-	vtable[Index] = Func;
-
-	return OldFunc;
-}
+enum{
+	// D3DDevice
+	D3DVTableIndex_DeviceCreateTexture = 20,
+	//D3DTexture
+	D3DVTableIndex_TextureGetLevelDesc = 14,
+	D3DVTableIndex_TextureLockRect     = 16,
+	D3DVTableIndex_TextureUnlockRect   = 17
+};
 
 /*
- * Texture that is currently locked. In theory there could be multiple but the way unreal handles this there's always only one
- * so no need for anything fancy.
- */
-static struct{
-	void* Pixels;
-	INT Width;
-	INT Height;
-} LockedTexture;
-
-/*
- * Functions which are used to patch the d3d object vtables.
- * We could simply inherit from the d3d interfaces but this way it is simpler and doesn't add any external dependencies.
+ * Direct3D types and Functions which are used to patch the d3d object vtables.
+ * We could simply include d3d8.h and inherit from the d3d interfaces but this way it is simpler and doesn't add any external dependencies.
  */
 
-/*
- * LockRect
- */
+typedef void FD3DDevice;
+typedef void FD3DTexture;
 
-struct D3DLockedRect{
-	INT Pitch;
+enum ED3DFormat{
+	D3DFormat_L6V5U5   = 61,
+	D3DFormat_X8L8U8V8 = 62
+};
+
+struct FD3DLockedRect{
+	INT   Pitch;
 	void* pBits;
 };
 
-typedef HRESULT(__stdcall*D3DLockRectFunc)(void*, UINT, D3DLockedRect*, const RECT*, DWORD);
-
-D3DLockRectFunc D3DLockRect = NULL;
-
-static HRESULT __stdcall LockRectOverride(void* D3DTexture, UINT Level, D3DLockedRect* pLockedRect, const RECT* pRect, DWORD Flags){
-	check(D3DLockRect);
-
-	HRESULT Result = D3DLockRect(D3DTexture, Level, pLockedRect, pRect, Flags);
-
-	if(SUCCEEDED(Result) && (LockedTexture.Width + LockedTexture.Height) > 0){
-		LockedTexture.Pixels = pLockedRect->pBits;
-	}else{
-		LockedTexture.Pixels = NULL;
-		LockedTexture.Width = 0;
-		LockedTexture.Height = 0;
-	}
-
-	return Result;
-}
-
 /*
- * LockRect
+ * L6V5U5 to X8L8U8V8 texture format conversion.
+ *
+ * L6V5U5 is not supported on newer hardwareand either makes the game and editor crash or the driver
+ * it directly to X8L8U8V8 which results in strange visual artifacts. That's why we detect if a texture is created with the L6V5U5 format and perform
+ * a proper conversion to X8L8U8V8.
  */
 
-typedef HRESULT(__stdcall*D3DUnlockRectFunc)(void*, UINT);
-
-D3DUnlockRectFunc D3DUnlockRect = NULL;
-
-struct L6U5V5Pixel{
-	short V:5;
-	short U:5;
-	unsigned short L:6;
-	//short pad;
+struct FTextureMipLevel{
+	UINT  Width;
+	UINT  Height;
+	void* Pixels;
 };
 
-struct X8L8U8V8Pixel{
-	INT8 V;
-	INT8 U;
+// Realistically there will always be only one current texture, but just to stay flexible we allow for multiple.
+static TArray<FD3DTexture*>              CurrentTextures;
+static TArray<INT>                       AvailableTextureIndices;
+static TArray<TArray<FTextureMipLevel> > MipLevelsByTexture;
+
+struct FL6U5V5Pixel{
+	short          V:5;
+	short          U:5;
+	unsigned short L:6;
+};
+
+struct FX8L8U8V8Pixel{
+	INT8  V;
+	INT8  U;
 	UINT8 L;
 	UINT8 X;
 };
 
-UINT8 Map6BitUnsignedTo8BitUnsigned(UINT8 b6){
-	return (UINT8)(b6 * 255 / 63);
+static UINT8 Map6BitUnsignedTo8BitUnsigned(UINT8 U6){
+	return (UINT8)(U6 * 255 / 63);
 }
 
-INT8 Map5BitSignedTo8BitSigned(INT8 b5){
+static INT8 Map5BitSignedTo8BitSigned(INT8 S5){
 	const int min5 = -16;
 	const int max5 = 15;
 	const int range5 = max5 - min5;
@@ -100,78 +74,192 @@ INT8 Map5BitSignedTo8BitSigned(INT8 b5){
 	const int max8 = 127;
 	const int range8 = max8 - min8;
 
-	return (INT8)((b5 - min5) * range8 / range5 + min8);
+	return (INT8)((S5 - min5) * range8 / range5 + min8);
 }
 
-static HRESULT __stdcall UnlockRectOverride(void* D3DTexture, UINT Level){
-	check(D3DUnlockRect);
+static void ConvertL6U5V5ToX8L8U8V8(const void* In, void* Out, UINT Width, UINT Height){
+	for(UINT Y = 0; Y < Height; ++Y){
+		for(UINT X = 0; X < Width; ++X){
+			INT Index = Y * Width + X;
+			const FL6U5V5Pixel* P1 = static_cast<const FL6U5V5Pixel*>(In) + Index;
+			FX8L8U8V8Pixel* P2 = static_cast<FX8L8U8V8Pixel*>(Out) + Index;
 
-	// Converting locked texture pixels from L6V5U5 to X8L8U8V8
-	if(LockedTexture.Pixels){
-		INT BufferSize = LockedTexture.Width * LockedTexture.Height * sizeof(X8L8U8V8Pixel);
-		void* TempBuffer = appMalloc(BufferSize);
+			P2->V = Map5BitSignedTo8BitSigned(P1->V);
+			P2->U = Map5BitSignedTo8BitSigned(P1->U);
+			P2->L = Map6BitUnsignedTo8BitUnsigned(P1->L);
+			P2->X = P2->L; // L6V5U5 only has one luminance value so we can just reuse it
+		}
+	}
+}
 
-		appMemcpy(TempBuffer, LockedTexture.Pixels, BufferSize);
 
-		for(INT y = 0; y < LockedTexture.Height; ++y){
-			for(INT x = 0; x < LockedTexture.Width; ++x){
-				// I have absolutely no idea what the multiplication with 2 does but that's what made it work somehow...
-				L6U5V5Pixel* P1 = static_cast<L6U5V5Pixel*>(TempBuffer) + (y * LockedTexture.Width * 2 + x);
-				X8L8U8V8Pixel* P2 = static_cast<X8L8U8V8Pixel*>(LockedTexture.Pixels) + y * LockedTexture.Width + x;
+/*
+ * GetLevelDesc
+ *
+ * Not overridden but used to get information about the size of the different mip levels.
+ */
 
-				P2->V = Map5BitSignedTo8BitSigned(P1->V);
-				P2->U = Map5BitSignedTo8BitSigned(P1->U);
-				P2->L = Map6BitUnsignedTo8BitUnsigned(P1->L);
-				P2->X = Map6BitUnsignedTo8BitUnsigned(P1->L);
+struct FD3DSurfaceDesc {
+	ED3DFormat               Format;
+	enum ED3DResourceType    Type;
+	DWORD                    Usage;
+	enum ED3DPool            Pool;
+	UINT                     Size;
+	enum ED3DMultiSampleType MultiSampleType;
+	UINT                     Width;
+	UINT                     Height;
+};
+
+typedef HRESULT(__stdcall*D3DTextureGetLevelDescFunc)(FD3DTexture*, UINT, FD3DSurfaceDesc*);
+
+D3DTextureGetLevelDescFunc D3DTextureGetLevelDesc = NULL;
+
+/*
+ * LockRect
+ */
+
+typedef HRESULT(__stdcall*D3DTextureLockRectFunc)(FD3DTexture*, UINT, FD3DLockedRect*, const RECT*, DWORD);
+
+D3DTextureLockRectFunc D3DTextureLockRect = NULL;
+
+static HRESULT __stdcall D3DTextureLockRectOverride(FD3DTexture* D3DTexture, UINT Level, FD3DLockedRect* pLockedRect, const RECT* pRect, DWORD Flags){
+	check(D3DTextureLockRect);
+	check(D3DTextureGetLevelDesc);
+
+	INT Index = CurrentTextures.FindItemIndex(D3DTexture);
+
+	if(Index != INDEX_NONE){
+		FD3DSurfaceDesc SurfaceDesc;
+
+		if(SUCCEEDED(D3DTextureGetLevelDesc(D3DTexture, Level, &SurfaceDesc))){
+			TArray<FTextureMipLevel>& MipLevels = MipLevelsByTexture[Index];
+			FTextureMipLevel& MipLevel = MipLevels[Level];
+
+			MipLevel.Width = SurfaceDesc.Width;
+			MipLevel.Height = SurfaceDesc.Height;
+			MipLevel.Pixels = appMalloc(SurfaceDesc.Size);
+
+			pLockedRect->Pitch = MipLevel.Width * sizeof(FL6U5V5Pixel);
+			pLockedRect->pBits = MipLevel.Pixels;
+
+			return S_OK;
+		}
+	}
+
+	return D3DTextureLockRect(D3DTexture, Level, pLockedRect, pRect, Flags);
+}
+
+/*
+ * UnlockRect
+ */
+
+typedef HRESULT(__stdcall*D3DTextureUnlockRectFunc)(FD3DTexture*, UINT);
+
+D3DTextureUnlockRectFunc D3DTextureUnlockRect = NULL;
+
+static HRESULT __stdcall D3DTextureUnlockRectOverride(FD3DTexture* D3DTexture, UINT Level){
+	check(D3DTextureUnlockRect);
+
+	INT TextureIndex = CurrentTextures.FindItemIndex(D3DTexture);
+
+	if(TextureIndex != INDEX_NONE){
+		check(MipLevelsByTexture.IsValidIndex(TextureIndex));
+		check(AvailableTextureIndices.FindItemIndex(TextureIndex) == INDEX_NONE);
+		check(MipLevelsByTexture[TextureIndex].IsValidIndex(Level));
+
+		FTextureMipLevel& MipLevel = MipLevelsByTexture[TextureIndex][Level];
+
+		FD3DLockedRect LockedRect;
+		HRESULT Result = D3DTextureLockRect(D3DTexture, Level, &LockedRect, NULL, 0);
+
+		if(SUCCEEDED(Result)){
+			ConvertL6U5V5ToX8L8U8V8(MipLevel.Pixels, LockedRect.pBits, MipLevel.Width, MipLevel.Height);
+
+			Result = D3DTextureUnlockRect(D3DTexture, Level);
+		}
+
+		appFree(MipLevel.Pixels);
+		MipLevel.Pixels = NULL;
+
+		if(Level == MipLevelsByTexture[TextureIndex].Num() - 1){
+			MipLevelsByTexture[TextureIndex].Empty();
+
+			if(TextureIndex == CurrentTextures.Num() - 1){
+				CurrentTextures.Pop();
+				MipLevelsByTexture.Pop();
+			}else{
+				CurrentTextures[TextureIndex] = NULL;
+				AvailableTextureIndices.AddItem(TextureIndex);
 			}
 		}
 
-		appFree(TempBuffer);
-
-		LockedTexture.Pixels = NULL;
-		LockedTexture.Width = 0;
-		LockedTexture.Height = 0;
+		return Result;
 	}
 
-	return D3DUnlockRect(D3DTexture, Level);
+	return D3DTextureUnlockRect(D3DTexture, Level);
 }
 
 /*
  * CreateTexture
  */
 
-typedef HRESULT(__stdcall*D3DCreateTextureFunc)(void*, UINT, UINT, UINT, DWORD, enum D3DFormat, enum D3DPool, void**);
+typedef HRESULT(__stdcall*D3DDeviceCreateTextureFunc)(FD3DDevice*, UINT, UINT, UINT, DWORD, ED3DFormat, enum ED3DPool, FD3DTexture**);
 
-D3DCreateTextureFunc D3DCreateTexture = NULL;
+D3DDeviceCreateTextureFunc D3DDeviceCreateTexture = NULL;
 
-static HRESULT __stdcall CreateTextureOverride(void* D3DDevice,
-											   UINT Width,
-											   UINT Height,
-											   UINT Levels,
-											   DWORD Usage,
-											   enum D3DFormat Format,
-											   enum D3DPool Pool,
-											   void** ppTexture){
-	check(D3DCreateTexture);
+static HRESULT __stdcall D3DDeviceCreateTextureOverride(FD3DDevice* D3DDevice,
+														UINT Width,
+														UINT Height,
+														UINT Levels,
+														DWORD Usage,
+														ED3DFormat Format,
+														enum ED3DPool Pool,
+														FD3DTexture** ppTexture){
+	check(D3DDeviceCreateTexture);
 
-	if(Format == 61) { // D3DFMT_L6V5U5 = 61
-		Format = static_cast<enum D3DFormat>(62); // D3DFMT_X8L8V8U8 = 62
-
-		// It is assumed that the texture is immediately locked after creation (which it is) so these dimensions will always refer to the correct texture
-		LockedTexture.Width = Width;
-		LockedTexture.Height = Height;
-	}
-
-	HRESULT Result = D3DCreateTexture(D3DDevice, Width, Height, Levels, Usage, Format, Pool, ppTexture);
+	HRESULT Result = D3DDeviceCreateTexture(D3DDevice,
+											Width,
+											Height,
+											Levels,
+											Usage,
+											Format == D3DFormat_L6V5U5 ? D3DFormat_X8L8U8V8 : Format,
+											Pool,
+											ppTexture);
 
 	if(SUCCEEDED(Result)){
-		if(!D3DLockRect)
-			D3DLockRect = static_cast<D3DLockRectFunc>(PatchVTable(*reinterpret_cast<void***>(*ppTexture), 16, LockRectOverride));
+		// Patching vtables if it wasn't done already
 
-		if(!D3DUnlockRect)
-			D3DUnlockRect = static_cast<D3DUnlockRectFunc>(PatchVTable(*reinterpret_cast<void***>(*ppTexture), 17, UnlockRectOverride));
+		if(!D3DTextureGetLevelDesc)
+			D3DTextureGetLevelDesc = static_cast<D3DTextureGetLevelDescFunc>((*reinterpret_cast<void***>(*ppTexture))[D3DVTableIndex_TextureGetLevelDesc]);
+
+		if(!D3DTextureLockRect){
+			D3DTextureLockRect = static_cast<D3DTextureLockRectFunc>(PatchVTable(*reinterpret_cast<void***>(*ppTexture),
+																				 D3DVTableIndex_TextureLockRect,
+																				 D3DTextureLockRectOverride));
+		}
+
+		if(!D3DTextureUnlockRect){
+			D3DTextureUnlockRect = static_cast<D3DTextureUnlockRectFunc>(PatchVTable(*reinterpret_cast<void***>(*ppTexture),
+																					 D3DVTableIndex_TextureUnlockRect,
+																					 D3DTextureUnlockRectOverride));
+		}
+
+		if(Format == D3DFormat_L6V5U5){
+			INT TextureIndex;
+
+			if(AvailableTextureIndices.Num() > 0) {
+				TextureIndex = AvailableTextureIndices.Pop();
+				CurrentTextures[TextureIndex] = *ppTexture;
+			}else{
+				TextureIndex = CurrentTextures.AddItem(*ppTexture);
+			}
+
+			if(MipLevelsByTexture.Num() <= TextureIndex)
+				MipLevelsByTexture.Set(TextureIndex + 1);
+
+			MipLevelsByTexture[CurrentTextures.FindItemIndex(*ppTexture)].Set(Levels);
+		}
 	}
-
 
 	return Result;
 }
@@ -188,8 +276,11 @@ public:
 	virtual UBOOL SetRes(UViewport* Viewport, INT NewX, INT NewY, UBOOL Fullscreen, INT ColorBytes = 0, UBOOL bSaveSize = true){
 		UBOOL result = UD3DRenderDevice::SetRes(Viewport, NewX, NewY, Fullscreen, ColorBytes, bSaveSize);
 
-		if(result && !D3DCreateTexture)
-			D3DCreateTexture = static_cast<D3DCreateTextureFunc>(PatchVTable(*reinterpret_cast<void***>(Direct3DDevice8), 20, CreateTextureOverride));
+		if(result && !D3DDeviceCreateTexture){
+			D3DDeviceCreateTexture = static_cast<D3DDeviceCreateTextureFunc>(PatchVTable(*reinterpret_cast<void***>(Direct3DDevice8),
+																						 D3DVTableIndex_DeviceCreateTexture,
+																						 D3DDeviceCreateTextureOverride));
+		}
 
 		return result;
 	}

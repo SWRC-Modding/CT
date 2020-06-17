0bad0ba4643bd962356ec1032dc426927140ee26
Leon Buckel <leon@buckel-baehr.de>
2020-06-17 12:04:04 +0200

    Fix UModel class size

--- Engine/Inc/UnModel.h
+++ Engine/Inc/UnModel.h
@@ -6,6 +6,227 @@
 		* Created by Tim Sweeney
 =============================================================================*/
 
+//
+//	FBspVertex
+//
+
+struct ENGINE_API FBspVertex{
+	FVector Position;
+	FVector Normal;
+	FLOAT   U,  V;
+	FLOAT   U2, V2;
+
+	// Serializer.
+
+	void Serialize(FArchive& Ar);
+};
+
+//
+//	FBspVertexStream
+//
+
+class FBspVertexStream : public FVertexStream{
+public:
+	TArray<FBspVertex> Vertices;
+
+	// Constructor.
+
+	FBspVertexStream();
+
+	// FVertexStream interface.
+
+	virtual INT GetSize();
+	virtual INT GetStride();
+	virtual INT GetComponents(FVertexComponent* OutComponents);
+	virtual void GetStreamData(void* Dest);
+	virtual void GetRawStreamData(void** Dest,INT FirstVertex);
+
+	// FRenderResource interface.
+
+	virtual QWORD GetCacheId();
+	virtual INT GetRevision();
+
+	// Serializer.
+
+	void Serialize(FArchive& Ar);
+};
+
+//
+//	FLightBitmap
+//
+
+class FLightBitmap{
+public:
+	AActor*      LightActor;
+	TArray<BYTE> Bits;
+	INT          SizeX, SizeY;
+	INT          MinX, MinY;
+	INT          MaxX, MaxY;
+	INT          OffsetX, OffsetY; // Offset into framebuffer when rendering light visibility: internal use only.
+	INT          Stride;
+
+	// Constructors.
+
+	FLightBitmap();
+	FLightBitmap(AActor* InActor, INT InMinX, INT InMinY, INT InMaxX, INT InMaxY);
+
+	// Serializer.
+
+	void Serialize(FArchive& Ar);
+};
+
+//
+//	FLightMap
+//
+
+class ENGINE_API FLightMap : public FTexture{
+public:
+	ULevel*              Level;
+	INT                  iTexture, iSurf, iZone;
+	INT                  OffsetX, OffsetY;
+	INT                  SizeX, SizeY;
+	FMatrix              WorldToLightMap;
+	FVector              LightMapBase, LightMapX, LightMapY;
+	TArray<FLightBitmap> Bitmaps;
+	TArray<AActor*>      DynamicLights;
+
+	// Constructors.
+
+	FLightMap(){}
+	FLightMap(ULevel* InLevel, INT InSurfaceIndex, INT InZoneIndex);
+
+	// FTexture interface.
+
+	virtual void* GetRawTextureData(INT MipIndex){ return NULL; }
+	virtual void UnloadRawTextureData(INT MipIndex){}
+	virtual void GetTextureData(INT MipIndex, void* Dest, INT DestStride, ETextureFormat DestFormat, UBOOL ColoredMips);
+	virtual UTexture* GetUTexture(){ return NULL; }
+
+	// FBaseTexture interface.
+
+	virtual INT GetWidth(){ return SizeX; }
+	virtual INT GetHeight(){ return SizeY; }
+	virtual INT GetFirstMip(){ return 0; }
+	virtual INT GetNumMips(){ return 1; }
+	virtual ETextureFormat GetFormat(){ return TEXF_RGBA8; }
+
+	virtual ETexClampMode GetUClamp(){ return TC_Clamp; }
+	virtual ETexClampMode GetVClamp(){ return TC_Clamp; }
+
+	// FRenderResource interface.
+
+	virtual QWORD GetCacheId(){ return 0; }
+	virtual INT GetRevision(){ return Revision; }
+
+	// Serializer.
+
+	void Serialize(FArchive& Ar);
+};
+
+//
+//	FStaticLightMapTexture
+//
+
+class ENGINE_API FStaticLightMapTexture : public FTexture{
+public:
+	TLazyArray<BYTE> Data[2]; // Two mip maps.
+	BYTE             Format;
+	INT              Width, Height;
+
+	// Constructors.
+
+	FStaticLightMapTexture();
+
+	// FTexture interface.
+
+	virtual void* GetRawTextureData(INT MipIndex);
+	virtual void UnloadRawTextureData(INT MipIndex);
+	virtual void GetTextureData(INT MipIndex,void* Dest, INT DestStride, ETextureFormat DestFormat, UBOOL ColoredMips);
+	virtual UTexture* GetUTexture(){ return NULL; }
+
+	// FBaseTexture interface.
+
+	virtual INT GetWidth(){ return Width; }
+	virtual INT GetHeight(){ return Height; }
+	virtual INT GetFirstMip();
+	virtual INT GetNumMips(){ return 2; }
+	virtual ETextureFormat GetFormat(){ return (ETextureFormat) Format; }
+	virtual ETexClampMode GetUClamp(){ return TC_Wrap; }
+	virtual ETexClampMode GetVClamp(){ return TC_Wrap; }
+
+	// FRenderResource interface.
+
+	virtual QWORD GetCacheId(){ return CacheId; }
+	virtual INT GetRevision(){ return Revision; }
+
+	// Serializer.
+
+	void Serialize(FArchive& Ar);
+};
+
+//
+//	FLightMapTexture
+//
+
+class ENGINE_API FLightMapTexture : public FCompositeTexture{
+public:
+	ULevel*                Level;
+	TArray<INT>            LightMaps;
+	FStaticLightMapTexture StaticTexture;
+
+	// Constructors.
+
+	FLightMapTexture() {}
+	FLightMapTexture(ULevel* InLevel);
+
+	// FCompositeTexture interface.
+
+	virtual INT GetNumChildren() { return LightMaps.Num(); }
+	virtual FTexture* GetChild(INT ChildIndex,INT* OutChildX,INT* OutChildY);
+
+	// FBaseTexture interface.
+
+	virtual INT GetWidth();//{ return LIGHTMAP_TEXTURE_WIDTH; }
+	virtual INT GetHeight();//{ return LIGHTMAP_TEXTURE_HEIGHT; }
+	virtual INT GetFirstMip() { return 0; }
+	virtual INT GetNumMips() { return 1; }
+	virtual ETextureFormat GetFormat() { return TEXF_RGBA8; }
+	virtual ETexClampMode GetUClamp() { return TC_Wrap; }
+	virtual ETexClampMode GetVClamp() { return TC_Wrap; }
+
+	// FRenderResource interface.
+
+	virtual QWORD GetCacheId() { return CacheId; }
+	virtual INT GetRevision() { return Revision; }
+
+	// Serializer.
+
+	void Serialize(FArchive& Ar);
+};
+
+//
+//	FBspSection
+//
+
+class ENGINE_API FBspSection{
+public:
+	FBspVertexStream Vertices;
+	INT              NumNodes;
+
+	UMaterial*       Material;
+	DWORD            PolyFlags;
+
+	INT              iLightMapTexture;
+
+	// Constructor.
+
+	FBspSection();
+
+	// Serializer.
+
+	void Serialize(FArchive& Ar);
+};
+
 /*-----------------------------------------------------------------------------
 	UModel.
 -----------------------------------------------------------------------------*/
@@ -15,224 +236,70 @@
 //
 enum {MAX_NODES  = 65536};
 enum {MAX_POINTS = 128000};
-class ENGINE_API UModel : public UPrimitive
-{
-#ifndef NODECALS
+class ENGINE_API UModel : public UPrimitive{
 	DECLARE_CLASS(UModel,UPrimitive,0,Engine)
-#else
-	DECLARE_CLASS(UModel,UPrimitive,CLASS_RuntimeStatic,Engine)
-#endif /*NODECALS*/
 
 	// Arrays and subobjects.
-	UPolys*					Polys;
-	TTransArray<FBspNode>	Nodes;
-	TTransArray<FVert>      Verts;
-	TTransArray<FVector>	Vectors;
-	TTransArray<FVector>	Points;
-	TTransArray<FBspSurf>	Surfs;
-	TArray<FLightMapIndex>	LightMap;
-	TArray<BYTE>			LightBits;
-	TArray<FBox>			Bounds;
-	TArray<INT>				LeafHulls;
-	TArray<FLeaf>			Leaves;
-	TArray<AActor*>			Lights;
+	UPolys*                  Polys;
+	TTransArray<FBspNode>    Nodes;
+	TTransArray<FVert>       Verts;
+	TTransArray<FVector>     Vectors;
+	TTransArray<FVector>     Points;
+	TTransArray<FBspSurf>    Surfs;
+	TArray<FBox>             Bounds;
+	TArray<INT>              LeafHulls;
+	TArray<FLeaf>            Leaves;
+	TArray<AActor*>          Lights;
+
+	TArray<FBspSection>      Sections;
+	TArray<FLightMapTexture> LightMapTextures;
+	TArray<FLightMap>        LightMaps;
+
+	TArray<INT>              DynamicLightMaps;
 
 	// Other variables.
-	UBOOL					RootOutside;
-	UBOOL					Linked;
-	INT						MoverLink;
-	INT						NumSharedSides;
-	INT						NumZones;
-	FZoneProperties			Zones[FBspNode::MAX_ZONES];
+	UBOOL                    RootOutside;
+	UBOOL                    Linked;
+	INT                      MoverLink;
+	INT                      NumSharedSides;
+	INT                      NumZones;
+	FZoneProperties          Zones[FBspNode::MAX_ZONES];
 
 	// Constructors.
-	UModel() : RootOutside(1),
-			   Surfs(this),
-			   Vectors(this),
-			   Points(this),
-			   Verts(this),
-			   Nodes(this){
-		EmptyModel( 1, 0 );
-	}
-	UModel( ABrush* Owner, UBOOL InRootOutside=1 );
+	UModel();
+	UModel(ABrush* Owner, UBOOL InRootOutside = 1);
 
 	// UObject interface.
-	void Serialize( FArchive& Ar );
+	void Serialize(FArchive& Ar);
 	void PostLoad();
 
 	// UPrimitive interface.
-	UBOOL PointCheck
-	(
-		FCheckResult	&Result,
-		AActor			*Owner,
-		FVector			Location,
-		FVector			Extent,
-		DWORD           ExtraNodeFlags
-	);
-	UBOOL LineCheck
-	(
-		FCheckResult	&Result,
-		AActor			*Owner,
-		FVector			End,
-		FVector			Start,
-		FVector			Extent,
-		DWORD           ExtraNodeFlags
-	);
-	FBox GetCollisionBoundingBox( const AActor *Owner ) const;
-	FBox GetRenderBoundingBox( const AActor* Owner, UBOOL Exact );
+	UBOOL PointCheck(FCheckResult& Result, AActor* Owner, FVector Location, FVector Extent, DWORD ExtraNodeFlags);
+	UBOOL LineCheck(FCheckResult& Result, AActor* Owner, FVector End, FVector Start, FVector Extent, DWORD ExtraNodeFlags);
+	FBox GetCollisionBoundingBox(const AActor *Owner) const;
+	FBox GetRenderBoundingBox(const AActor* Owner, UBOOL Exact);
 
 	// UModel interface.
-	void Modify( UBOOL DoTransArrays=0 );
+	void Modify(UBOOL DoTransArrays = 0);
 	void BuildBound();
-	void Transform( ABrush* Owner );
-	void EmptyModel( INT EmptySurfInfo, INT EmptyPolys );
+	void Transform(ABrush* Owner);
+	void EmptyModel(INT EmptySurfInfo, INT EmptyPolys);
 	void ShrinkModel();
-	UBOOL PotentiallyVisible( INT iLeaf1, INT iLeaf2 );
-	BYTE FastLineCheck( FVector End, FVector Start );
+	UBOOL PotentiallyVisible(INT iLeaf1, INT iLeaf2);
+	BYTE FastLineCheck(FVector End, FVector Start);
 
 	// UModel transactions.
-	void ModifySelectedSurfs( UBOOL UpdateMaster );
-	void ModifyAllSurfs( UBOOL UpdateMaster );
-	void ModifySurf( INT Index, UBOOL UpdateMaster );
+	void ModifySelectedSurfs(UBOOL UpdateMaster);
+	void ModifyAllSurfs(UBOOL UpdateMaster);
+	void ModifySurf(INT Index, UBOOL UpdateMaster);
 
 	// UModel collision functions.
 	typedef void (*PLANE_FILTER_CALLBACK )(UModel *Model, INT iNode, int Param);
 	typedef void (*SPHERE_FILTER_CALLBACK)(UModel *Model, INT iNode, int IsBack, int Outside, int Param);
-	FPointRegion PointRegion(class AZoneInfo* Zone, class FVector const & Location)const;
-	FLOAT FindNearestVertex
-	(
-		const FVector	&SourcePoint,
-		FVector			&DestPoint,
-		FLOAT			MinRadius,
-		INT				&pVertex
-	) const;
-	void PrecomputeSphereFilter
-	(
-		const FPlane	&Sphere
-	);
-	FLightMapIndex* GetLightMapIndex( INT iSurf )
-	{
-		guard(UModel::GetLightMapIndex);
-		if( iSurf == INDEX_NONE )
-			return NULL;
-
-		FBspSurf& Surf = Surfs[iSurf];
-
-		if( Surf.iLightMap==INDEX_NONE || !LightMap.Num() )
-			return NULL;
-
-		return &LightMap[Surf.iLightMap];
-		unguard;
-	}
-};
-
-class ENGINE_API UBspNodes : public UObject
-{
-	DECLARE_CLASS(UBspNodes,UObject,CLASS_RuntimeStatic,Engine)
-	TArray<FBspNode> Element;
-	INT _NumZones;
-	FZoneProperties	_Zones[FBspNode::MAX_ZONES];
-	UBspNodes()
-	//: Element( this )
-	{
-		guard(UBspNodes::UBspNodes);
-		_NumZones = 0;
-		for( INT i=0; i<FBspNode::MAX_ZONES; i++ )
-		{
-			_Zones[i].ZoneActor    = NULL;
-			_Zones[i].Connectivity = ((QWORD)1)<<i;
-			_Zones[i].Visibility   = ~(QWORD)0;
-		}	
-		unguard;
-	}
-	void Serialize( FArchive& Ar )
-	{
-		guard(UBspNodes::Serialize);
-		Super::Serialize(Ar);
-		if( Ar.IsLoading() )
-		{
-			INT DbNum=Element.Num(), DbMax=DbNum;
-			Ar << DbNum << DbMax;
-			Element.Empty( DbNum );
-			Element.AddZeroed( DbNum );
-
-			for( INT i=0; i<Element.Num(); i++ )
-				Ar << Element[i];
-
-			Ar << AR_INDEX(_NumZones);
-
-			for( INT i=0; i<_NumZones; i++ )
-				Ar << _Zones[i];
-		}
-		unguardobj;
-	}
-};
-
-class ENGINE_API UBspSurfs : public UObject
-{
-	DECLARE_CLASS(UBspSurfs,UObject,CLASS_RuntimeStatic,Engine)
-	TArray<FBspSurf> Element;
-	void Serialize( FArchive& Ar )
-	{
-		guard(FBspSurfs::Serialize);
-		Super::Serialize( Ar );
-		if( Ar.IsLoading() )
-		{
-			INT DbNum=0, DbMax=0;
-			Ar << DbNum << DbMax;
-			Element.Empty( DbNum );
-			Element.AddZeroed( DbNum );
-			for( INT i=0; i<Element.Num(); i++ )
-				Ar << Element[i];
-		}
-		unguard;
-	}
-};
-
-class ENGINE_API UVectors : public UObject
-{
-	DECLARE_CLASS(UVectors,UObject,CLASS_RuntimeStatic,Engine)
-	TArray<FVector> Element;
-	void Serialize( FArchive& Ar )
-	{
-		guard(UVectors::Serialize);
-		Super::Serialize( Ar );
-		if( Ar.IsLoading() )
-		{
-			INT DbNum=Element.Num(), DbMax=DbNum;
-			Ar << DbNum << DbMax;
-			Element.Empty( DbNum );
-			Element.Add( DbNum );
-
-			for( INT i=0; i<Element.Num(); i++ )
-				Ar << Element[i];
-		}
-		unguard;
-	}
-};
-
-class ENGINE_API UVerts : public UObject
-{
-	DECLARE_CLASS(UVerts,UObject,CLASS_RuntimeStatic,Engine)
-	TArray<FVert> Element;
-	INT NumSharedSides;
-	void Serialize( FArchive& Ar )
-	{
-		guard(UVerts::Serialize);
-		Super::Serialize( Ar );
-		if( Ar.IsLoading() )
-		{
-			Element.CountBytes( Ar );
-			INT DbNum=Element.Num(), DbMax=DbNum;
-			Ar << DbNum << DbMax;
-			Element.Empty( DbNum );
-			Element.Add( DbNum );
-			for( INT i=0; i<Element.Num(); i++ )
-				Ar << Element[i];
-		}
-		Ar << AR_INDEX(NumSharedSides);
-		unguardobj;
-	}
+	FPointRegion PointRegion(class AZoneInfo* Zone, const FVector& Location) const;
+	FLOAT FindNearestVertex(const FVector& SourcePoint, FVector& DestPoint, FLOAT MinRadius, INT& pVertex) const;
+	void PrecomputeSphereFilter(const FPlane& Sphere);
+	FLightMapIndex* GetLightMapIndex(INT iSurf);
 };
 
 /*----------------------------------------------------------------------------
--- Engine/Inc/UnPrim.h
+++ Engine/Inc/UnPrim.h
@@ -6,6 +6,8 @@
 		* Created by Tim Sweeney
 =============================================================================*/
 
+class UMaterial;
+
 /*-----------------------------------------------------------------------------
 	FCheckResult.
 -----------------------------------------------------------------------------*/
@@ -13,51 +15,47 @@
 //
 // Results of an actor check.
 //
-struct FIteratorActorList : public FIteratorList
-{
+struct FIteratorActorList : public FIteratorList{
 	// Variables.
 	AActor* Actor;
 
 	// Functions.
-	FIteratorActorList()
-	{}
-	FIteratorActorList( FIteratorActorList* InNext, AActor* InActor )
-	:	FIteratorList	(InNext)
-	,	Actor			(InActor)
-	{}
-	FIteratorActorList* GetNext()
-	{ return (FIteratorActorList*) Next; }
+	FIteratorActorList(){}
+
+	FIteratorActorList(FIteratorActorList* InNext, AActor* InActor) : FIteratorList(InNext),
+																	  Actor(InActor){}
+
+	FIteratorActorList* GetNext(){ return (FIteratorActorList*) Next; }
 };
 
 //
 // Results from a collision check.
 //
-struct FCheckResult : public FIteratorActorList
-{
+struct FCheckResult : public FIteratorActorList{
 	// Variables.
-	FVector		Location;   // Location of the hit in coordinate system of the returner.
-	FVector		Normal;     // Normal vector in coordinate system of the returner. Zero=none.
-	UPrimitive*	Primitive;  // Actor primitive which was hit, or NULL=none.
+	FVector     Location;   // Location of the hit in coordinate system of the returner.
+	FVector     Normal;     // Normal vector in coordinate system of the returner. Zero=none.
+	UPrimitive* Primitive;  // Actor primitive which was hit, or NULL=none.
 	FLOAT       Time;       // Time until hit, if line check.
-	INT			Item;       // Primitive data item which was hit, INDEX_NONE=none.
-
-	char Padding[32];	//For security...
+	INT         Item;       // Primitive data item which was hit, INDEX_NONE=none.
+	UMaterial*  Material;   // Material of the item which was hit.
 
 	// Functions.
-	FCheckResult()
-	{}
-	FCheckResult( FLOAT InTime, FCheckResult* InNext=NULL )
-	:	FIteratorActorList( InNext, NULL )
-	,	Location	(0,0,0)
-	,	Normal		(0,0,0)
-	,	Primitive	(NULL)
-	,	Time		(InTime)
-	,	Item		(INDEX_NONE)
-	{}
-	FCheckResult*& GetNext()
-		{ return *(FCheckResult**)&Next; }
-	friend QSORT_RETURN CDECL CompareHits( const FCheckResult* A, const FCheckResult* B )
-		{ return A->Time<B->Time ? -1 : A->Time>B->Time ? 1 : 0; }
+	FCheckResult(){}
+
+	FCheckResult(FLOAT InTime, FCheckResult* InNext = NULL) : FIteratorActorList(InNext, NULL),
+															  Location(0,0,0),
+															  Normal(0,0,0),
+															  Primitive(NULL),
+															  Time(InTime),
+															  Item(INDEX_NONE),
+															  Material(NULL){}
+
+	FCheckResult*& GetNext(){ return *(FCheckResult**)&Next; }
+
+	friend QSORT_RETURN CDECL CompareHits(const FCheckResult* A, const FCheckResult* B){
+		return A->Time < B->Time ? -1 : A->Time > B->Time ? 1 : 0;
+	}
 };
 
 /*-----------------------------------------------------------------------------
@@ -76,10 +74,8 @@ class ENGINE_API UPrimitive : public UObject{
 	FSphere BoundingSphere;
 
 	// Constructor.
-	UPrimitive()
-	: BoundingBox(0)
-	, BoundingSphere(0)
-	{}
+	UPrimitive() : BoundingBox(0),
+				   BoundingSphere(0){}
 
 	//UObject interface.
 	void Serialize( FArchive& Ar );
@@ -87,29 +83,29 @@ class ENGINE_API UPrimitive : public UObject{
 	//Virtual Functions
 	virtual UBOOL PointCheck
 	(
-		FCheckResult&	Result,
-		AActor*			Owner,
-		const FVector&	Location,
-		const FVector&	Extent,
-		DWORD           ExtraNodeFlags
+		FCheckResult&  Result,
+		AActor*        Owner,
+		const FVector& Location,
+		const FVector& Extent,
+		DWORD          ExtraNodeFlags
 	);
 	virtual UBOOL LineCheck
 	(
-		FCheckResult&	Result,
-		AActor*			Owner,
-		const FVector&	End,
-		const FVector&	Start,
-		const FVector&	Extent,
-		DWORD           ExtraNodeFlags,
-		DWORD			IDONTKNOWWHATTHISIS
+		FCheckResult&  Result,
+		AActor*        Owner,
+		const FVector& End,
+		const FVector& Start,
+		const FVector& Extent,
+		DWORD          ExtraNodeFlags,
+		DWORD          TraceFlags
 	);
 	virtual FBox GetRenderBoundingBox(const AActor* Owner);
 	virtual FSphere GetRenderBoundingSphere(const AActor* Owner);
 	virtual FBox GetCollisionBoundingBox(const AActor* Owner) const;
 	virtual int UseCylinderCollision(const AActor*);
-	virtual void Illuminate(AActor*, int);
-	virtual FVector GetEncroachExtent(AActor*);
-	virtual FVector GetEncroachCenter(AActor*);
+	virtual void Illuminate(AActor* Owner, UBOOL ChangedOnly = 0);
+	virtual FVector GetEncroachExtent(AActor* Owner);
+	virtual FVector GetEncroachCenter(AActor* Owner);
 };
 
 /*----------------------------------------------------------------------------

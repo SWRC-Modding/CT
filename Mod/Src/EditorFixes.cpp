cb617f9e32094f74e195eacb5c845a3b7d89dd3d
Leon Buckel <leon@buckel-baehr.de>
2020-09-26 13:20:32 +0200

    Add fix for property editing crash

--- Mod/Src/EditorFixes.cpp
+++ Mod/Src/EditorFixes.cpp
@@ -1,22 +1,50 @@
 #include "../../Engine/Inc/Engine.h"
 #include "../Inc/Mod.h"
 
-void(__fastcall*OriginalUEditorEngineTick)(void*, DWORD, FLOAT) = NULL;
+/*
+ * Property window crash fix
+ */
 
-void __fastcall UEditorEngineTickOverride(void* Self, DWORD edx, FLOAT DeltaTime){
+bool(__fastcall*OriginalUClassIsDefaultValue)(UObject* Self, DWORD, const FPropertyInstance&) = NULL;
+
+bool __fastcall UClassIsDefaultValueOverride(UObject* Self, DWORD edx, const FPropertyInstance& PropertyInstance){
+	for(INT i = 1; i < PropertyInstance.NestedProperties.Num(); ++i){
+		if(PropertyInstance.NestedProperties[i].Property->IsA(UStrProperty::StaticClass()))
+			return false;
+	}
+
+	return OriginalUClassIsDefaultValue(Self, edx, PropertyInstance);
+}
+
+/*
+ * UEditorEngine tick hook
+ */
+
+void(__fastcall*OriginalUEditorEngineTick)(USubsystem* Self, DWORD, FLOAT) = NULL;
+
+void __fastcall UEditorEngineTickOverride(USubsystem* Self, DWORD edx, FLOAT DeltaTime){
 	static bool appliedFixes = false;
 
 	if(!appliedFixes){
+		guard(ApplyEditorFixes)
 		appliedFixes = true;
 
 		GLog->Log("Applying crash fixes");
-		guard(ApplyEditorFixes)
 		/*
+		 * Fix 1:
 		 * Placing a FluidSurfaceInfo crashes because the constructor for FDynamicActor accesses its 'Skins' array which is empty by default.
 		 * To fix this we simply resize the property in the default actor so that it contains an element.
 		 */
 		AFluidSurfaceInfo::StaticClass()->GetDefaultActor()->Skins.Set(1);
-		// TODO: Add more
+		/*
+		 * Fix 2:
+		 * Expanding a struct that contains a string property in an object property window causes a crash while the editor is checking for modified values
+		 * in order to highlight them.
+		 * This is fixed here by simply checking if there is a nested string property and not calling the original code if there is.
+		 * As a downside this causes the string property to be drawn as bold text even though it may have not been modified but this shouldn't be a big deal
+		 * since that case is rather rare anyway.
+		 */
+		OriginalUClassIsDefaultValue = static_cast<bool(__fastcall*)(UObject*, DWORD, const FPropertyInstance&)>(PatchVTable(UClass::StaticClass(), 29, UClassIsDefaultValueOverride));
 		unguard;
 	}
 
@@ -30,7 +58,7 @@ void __fastcall UEditorEngineTickOverride(void* Self, DWORD edx, FLOAT DeltaTime
  */
 MOD_API struct EditorFixes{
 	EditorFixes(){
-		if(!GIsEditor)
+		if(!GIsEditor || GIsUCC)
 			return;
 
 		void* handle = appGetDllHandle(*(FStringTemp(appBaseDir()) * appPackage() + ".exe")); // This works even if the exe was renamed
@@ -38,7 +66,7 @@ MOD_API struct EditorFixes{
 		void** vfPtr = static_cast<void**>(appGetDllExport(handle, "??_7UUnrealEdEngine@@6BUObject@@@"));
 		check(vfPtr);
 
-		OriginalUEditorEngineTick = static_cast<void(__fastcall*)(void*, DWORD, FLOAT)>(PatchVTable(&vfPtr, 32, UEditorEngineTickOverride));
+		OriginalUEditorEngineTick = static_cast<void(__fastcall*)(USubsystem*, DWORD, FLOAT)>(PatchVTable(&vfPtr, 32, UEditorEngineTickOverride));
 
 		appFreeDllHandle(handle); // Not actually freeing, just decrementing the refCount in case that could break something later
 	}

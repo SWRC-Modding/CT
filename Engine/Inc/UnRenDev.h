f6669d799c5ae5643cf52de36b37c9e297b7667c
Leon Buckel <leon@buckel-baehr.de>
2020-10-29 18:22:47 +0100

    Use INT instead of WORD indices for selection

--- Engine/Inc/UnRenDev.h
+++ Engine/Inc/UnRenDev.h
@@ -85,7 +85,7 @@ public:
 	virtual void PushState(int) = 0;
 	virtual void PopState(int) = 0;
 	virtual UBOOL SetRenderTarget(FRenderTarget* RenderTarget, bool) = 0;
-	virtual UBOOL SetCubeRenderTarget(class FDynamicCubemap*, int, int){}
+	virtual UBOOL SetCubeRenderTarget(class FDynamicCubemap*, int, int){ return 0; }
 	virtual void SetViewport(INT X, INT Y, INT Width, INT Height) = 0;
 	virtual void Clear(UBOOL UseColor = 1, FColor Color = FColor(0, 0, 0), UBOOL UseDepth = 1, FLOAT Depth = 1.0f, UBOOL UseStencil = 1, DWORD Stencil = 0) = 0;
 	virtual void PushHit(const BYTE* Data, INT Count) = 0;
@@ -110,7 +110,7 @@ public:
 	virtual void CopyBackBufferToTarget(FAuxRenderTarget*){}
 	virtual void SetLODDiffuseFade(float){}
 	virtual void SetLODSpecularFade(float){}
-	virtual void SetStencilOp(ECompareFunction Test, DWORD Ref, DWORD Mask, EStencilOp FailOp, EStencilOp ZFailOp, EStencilOp PassOp, DWORD WriteMask);
+	virtual void SetStencilOp(ECompareFunction Test, DWORD Ref, DWORD Mask, EStencilOp FailOp, EStencilOp ZFailOp, EStencilOp PassOp, DWORD WriteMask){}
 	virtual void vtpad1(int) = 0; // Possibly stencil op related (modifies same memory);
 	virtual void vtpad2(int) = 0; // Possibly stencil op related (modifies same memory);
 	virtual void SetPrecacheMode(EPrecacheMode PrecacheMode) = 0;

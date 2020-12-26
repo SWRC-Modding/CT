584c1f90edb2ac3fdbead1fc239c67759295ac75
Leon Buckel <leon@buckel-baehr.de>
2020-12-26 21:39:51 +0100

    Remove leftovers from old shader system

--- OpenGLDrv/Inc/Shader.h
+++ OpenGLDrv/Inc/Shader.h
@@ -53,41 +53,12 @@ public:
 		++Revision;
 	}
 
-	void SetVertexShaderMain(const FString& InVertexShaderMain){
-		VertexShaderMain = InVertexShaderMain;
-		++Revision;
-	}
-
-	void SetFragmentShaderMain(const FString& InFragmentShaderMain){
-		FragmentShaderMain = InFragmentShaderMain;
-		++Revision;
-	}
-
 	const TCHAR* GetName() const{ return *Name; }
 	const TCHAR* GetVertexShaderText() const{ return *VertexShaderText; }
 	const TCHAR* GetFragmentShaderText() const{ return *FragmentShaderText; }
-
-	FStringTemp GetVertexShaderMain() const{
-		if(VertexShaderMain.Len() > 0)
-			return VertexShaderMain;
-
-		return FStringTemp("void main(void){\n", true) + "\tgl_Position = " + GetVertexShaderEntryPointName() + "();\n}\n";
-	}
-
-	FStringTemp GetFragmentShaderMain() const{
-		if(FragmentShaderMain.Len() > 0)
-			return FragmentShaderMain;
-
-		return FStringTemp("void main(void){\n", true) + "\tFragColor = " + GetFragmentShaderEntryPointName() + "();\n}\n";
-	}
-
-	FStringTemp GetVertexShaderEntryPointName() const{ return Name.Locs() + "_vs_main"; }
-	FStringTemp GetFragmentShaderEntryPointName() const{ return Name.Locs() + "_fs_main"; }
 
 private:
 	FString Name;
 	FString VertexShaderText;
 	FString FragmentShaderText;
-	FString VertexShaderMain;
-	FString FragmentShaderMain;
 };

379e138a493ee8c021cb677610c6d9489eb8a2d7
Leon Buckel <leon@buckel-baehr.de>
2021-03-14 13:08:26 +0100

    Add clean command to build.bat

--- build.bat
+++ build.bat
@@ -2,8 +2,12 @@
 
 if /I "%1" == "debug" (
 	set CONFIG=debug
+	set CMD=%2
 ) else (
 	set CONFIG=release
+	set CMD=%1
 )
 
-"%VS71COMNTOOLS%\..\IDE\devenv.com" /build %CONFIG% CT.sln
+if /I not "%CMD%" == "clean" set CMD="build"
+
+"%VS71COMNTOOLS%\..\IDE\devenv.com" /%CMD% %CONFIG% CT.sln
